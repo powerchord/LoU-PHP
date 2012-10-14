4fbf115a8d1c4cecbe0ad15c367498bcf0939c9f
PastorBones <pastor_bones@yahoo.com>
2012-10-14 15:32:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,128 @@
+LoU-PHP
+=======
+
+## Description
+
+LoU-PHP is an opensourced class that provides basic methods for connecting, authenticating, and retrieving data from [Lord of Ultima](http://www.lordofultima.com).
+
+## Requirements
+
+LoU-PHP requires the [Simple HTML DOM Parser](http://simplehtmldom.sourceforge.net/) library which is included in this GitHub project directory for your convenience. The file must be located within the same directory as the classfile.
+
+## Usage
+
+Before you start, you need to
+
+* create a writable directory for storage of cookie files
+* know the LoU account info (email, password, world id)
+
+### Connecting to LoU
+
+This class is extremely basic providing only the minimal methods needed for connecting to LoU and retrieving data from the LoU API. To login to a LoU server:
+
+```php
+
+$lou = new LoU('path/to/cookie/directory');
+$lou->login('myemail@domain.com', 'mypassword');
+```
+
+This will authenticate with LoU and populate a list of available servers. That server list can be shown like this:
+
+```php
+
+foreach( $lou->server_list as $world )
+{
+    echo 'ID: ' . $world->id . '<br>';
+    echo 'Name: ' . $world->name . '<br>';
+    echo 'URL: ' . $world->server . '<br>';
+}
+```
+
+There are several ways of selecting a LoU world and retrieving a valid session key. This allows you to connect to a world with one simple step:
+
+```php
+
+// Method 1
+$lou = new LoU('path/to/cookie/directory');
+$lou->setWord(74)->login('myemail@domain.com', 'mypassword')
+
+// Method 2
+$lou = new LoU('path/to/cookie/directory');
+$lou->login('myemail@domain.com', 'mypassword')->selectWorld(74);
+```
+
+Or you can first login and display a list of worlds for a user to choose from and then select the world:
+
+````php
+
+$lou = new LoU('path/to/cookie/directory');
+$lou->login('myemail@domain.com', 'mypassword');
+
+foreach( $lou->server_list as $world )
+{
+    echo 'ID: ' . $world->id . '<br>';
+    echo 'Name: ' . $world->name . '<br>';
+    echo 'URL: ' . $world->server . '<br>';
+}
+
+// Controls for user selection here
+
+$lou->selectWorld($user_choice);
+```
+
+### Retrieving Data
+
+There are two methods for retrieving data from LoU. The first method involves providing an EndPoint and any necessary data:
+
+```php
+
+// Retrieve public information for the given player id
+$data = $lou->get( 'GetPublicPlayerInfo', array( 'id' => '1234567' ) );
+
+// Retrieve public information for the given city id
+$data = $lou->get( 'GetPublicCityInfo', array( 'id' => '1234567' ) );
+```
+
+This method is for retrieving information via the Poll EndPoint:
+
+```php
+
+// Retrieve current list of incoming attacks
+$data = $lou->poll( array( 'ALL_AT' => '' ) );
+
+// Retrieve private information of the authenticated user's city
+$data = $lou->poll( array( 'CITY' => '1234567' ) );
+
+// Retrieve time information from the server
+$data = $lou->poll( array( 'TIME' => time() ) );
+```
+
+## What's Next?
+
+There are many endpoints used in LoU for sending commands and retrieving data. I'll leave that for you to explore. I do plan on extending this library with more classes providing simple methods for retrieving data.
+
+### Todo
+
+I have not been able to decode the data needed to request World Map info. If anybody can help me figure this out, I would be forever grateful. From my best understanding it is encoded using basE91 LE with the following cipher:
+
+```php
+
+$cipher = array (
+    'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7, 'I' => 8, 'J' => 9, 'K' => 10, 'L' => 11, 'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15, 'Q' => 16, 'R' => 17,
+    'S' => 18, 'T' => 19, 'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23, 'Y' => 24, 'Z' => 25, 'a' => 26, 'b' => 27, 'c' => 28, 'd' => 29, 'e' => 30, 'f' => 31, 'g' => 32, 'h' => 33, 'i' => 34,
+    'j' => 35, 'k' => 36, 'l' => 37, 'm' => 38, 'n' => 39, 'o' => 40, 'p' => 41, 'q' => 42, 'r' => 43, 's' => 44, 't' => 45, 'u' => 46, 'v' => 47, 'w' => 48, 'x' => 49, 'y' => 50, 'z' => 51,
+    '0' => 52, '1' => 53, '2' => 54, '3' => 55, '4' => 56, '5' => 57, '6' => 58, '7' => 59, '8' => 60, '9' => 61, '!' => 62, '#' => 63, '$' => 64, '%' => 65, '&' => 66, '(' => 67, ')' => 68,
+    '*' => 69, '+' => 70, ',' => 71, '.' => 72, ' ' => 73, ':' => 74, ';' => 75, '<' => 76, '=' => 77, '>' => 78, '?' => 79, '@' => 80, '[' => 81, ']' => 82, '^' => 83, '_' => 84, '`' => 85,
+    '{' => 86, '|' => 87, '}' => 88, '~' => 89, '\'' => 90
+);
+```
+
+## License
+
+Copyright 2012, Roger Mayfield
+
+This library is released under the [GNU General Public License](http://opensource.org/licenses/gpl-3.0.html)
+
+### Disclaimer
+
+Although using this class for data retrieval is not prohibited by the [Lord of Ultima Terms of Service](http://www.lordofultima.com/en/game/rules), it is against the rules to automate the game using the methods contained within it. You are liable for your own applications of this library. I am not responsible for any bans that may occur.
---
+++ lou.class.php
@@ -0,0 +1,268 @@
+<?php
+
+/**
+ * Lord of Ultima connection class.
+ * This class connects, authenticates, and provides basic methods
+ * for retrieving data from LoU via EndPoint designations.
+ * @author Roger Mayfield <pastor_bones@yahoo.com>
+ * @copyright Copyright (c) 2012, Roger Mayfield
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @version v0.0.1
+ */
+Class LoU
+{
+
+    /**
+     * Path where cookies should be saved.
+     * @var string
+     */
+    private $_cookie_path;
+
+    /**
+     * Name of our cookie file.
+     * @var string
+     */
+    private $_cookie_file;
+
+    /**
+     *  World ID of the LoU server to connect to
+     * @var int
+     */
+     private $_world_id;
+
+    /**
+     * The list of available LoU Servers
+     * @var mixed
+     */
+     public $server_list = array();
+
+    /**
+     * Contains all our session information
+     * @var mixed
+     */
+     private $_session;
+
+    /**
+     * Placeholder for error message.
+     * Contains short description of an error after the error happens.
+     * @var string
+     */
+    public $error = '';
+
+    /**
+     * Object Constructor
+     * @param string $cookie_path
+     * @return mixed
+     */
+    public function __construct( $cookie_path )
+    {
+        // Set cookie path
+        $this->_cookie_path = ( substr_compare( $cookie_path, '/', -1, strlen( $cookie_path ) ) === 0 ) ? $cookie_path . '/' : $cookie_path;
+        return $this;
+    }
+
+    /**
+     * Logins to LoU.
+     * Authenticates a LoU account by using an email and password. If the world id
+     * is set previously, it will open a session. Returns $this for method-chains.
+     * 
+     * @param string $email LoU account email
+     * @param string $pass LoU account password
+     * @return mixed
+     */
+    public function login( $email, $pass )
+    {
+        if( empty( $this->cookieFile ) )
+        {
+            $this->_cookie_file = $this->generateCookieFile( $email );
+        }
+
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, 'http://www.lordofultima.com/en/' );
+        curl_setopt( $ch, CURLOPT_REFERER, 'http://www.lordofultima.com/en/' );
+        curl_setopt( $ch, CURLOPT_COOKIEFILE, $this->_cookie_file );
+        curl_setopt( $ch, CURLOPT_COOKIEJAR, $this->_cookie_file );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+        curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
+        curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
+        curl_exec( $ch );
+
+        $fields = array( 'mail' => $email, 'password' => $pass );
+        $field_str = http_build_query( $fields );
+        curl_setopt( $ch, CURLOPT_URL, 'https://www.lordofultima.com/en/user/login/' );
+        curl_setopt( $ch, CURLOPT_POST, 1 );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $field_str );
+        $result = curl_exec( $ch );
+
+        if( strstr( $result, "fill in a correct email" ) )
+        {
+            $this->error = 'Invalid username and/or password.';
+            return false;
+        }
+
+        require_once( './simple_html_dom.php' );
+        $html = new simple_html_dom();
+        $html->load( $result );
+
+        $e = $html->find( '.server-list', 0 )->find( '.menu_bubble' );
+        $sessId = $e[ 0 ]->find( '#sessionId', 0 )->value;
+
+        foreach( $e as $v )
+        {
+            $world = new stdClass();
+            $world->name = $v->find( '.inner', 0 )->value;
+            preg_match( "/World (\d+)/i", $world->name, $match );
+            $world->id = $match[ 1 ];
+            preg_match( "/prodgame(\d+)\.lordofultima\.com\/(\d+)/i", $v->find( 'form', 0 )->action, $match );
+            $world->server = "http://prodgame" . $match[ 1 ] . ".lordofultima.com/" . $match[ 2 ];
+            
+            array_push( $this->server_list, $world );
+        }
+
+        $this->_session = new stdClass();
+        $this->_session->id = $sessId;
+
+        // Select world if given
+        if( !empty( $this->_world_id ) )
+        {
+            return $this->selectWorld( $this->_world_id );
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets the World ID to use when querying the LoU server.
+     * 
+     * @param int $world_id
+     * @return mixed
+     */
+    public function setWorld( $world_id )
+    {
+        if( !is_numeric( $world_id ) )
+        {
+            $this->error = 'Invalid World ID';
+            return false;
+        }
+        
+        $this->_world_id = $world_id;
+        return $this;
+    }
+
+    /**
+     * Selects the world using the given world id.
+     * 
+     * @param int $world_id
+     * @return mixed
+     */
+    public function selectWorld( $world_id )
+    {
+        if( !$this->setWorld( $world_id ) instanceof $this )
+        {
+            return false;
+        }
+
+        foreach($this->server_list as $world){
+            if( $world->id == $world_id )
+            {
+                if( !$sess_key = $this->getSessKey( $world->server, $this->_session->id ) )
+                {
+                    $this->error = 'Invalid server and/or session id';
+                    return false;
+                }
+                $this->_session->key = $sess_key;
+                $this->_session->world = $world->name;
+                $this->_session->server = $world->server;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieves LoU session key.
+     * @param string $sess_id
+     * @return string
+     */
+    public function getSessKey( $url, $sess_id )
+    {
+        $result = $this->getData( $url, 'OpenSession', array('session' => $sess_id, 'rest' => 'false' ) );
+        
+        if( isset( $result->i ) )
+        {
+            return $result->i;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves data from a given endpoint
+     * @param string $url
+     * @param string $endpoint
+     * @param mixed $data
+     * @return mixed
+     */
+    public function getData( $url, $endpoint, $data )
+    {
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, "$url/Presentation/Service.svc/ajaxEndpoint/$endpoint" );
+        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 2 );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Content-Type: application/json; charset=utf-8", "Cache-Control: no-cache", "Pragma: no-cache", "X-Qooxdoo-Response-Type: application/json" ) );
+        curl_setopt( $ch, CURLOPT_POST, true );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, str_replace( '\\\\', '\\', json_encode( $data ) ) );
+        curl_setopt( $ch, CURLOPT_COOKIEFILE, $this->_cookie_file );
+        curl_setopt( $ch, CURLOPT_COOKIEJAR, $this->_cookie_file );
+        $result = curl_exec( $ch );
+        curl_close( $ch );
+
+        if( empty( $result ) )
+        {
+            $this->error = 'LoU returned an empty result';
+            return false;
+        }
+        
+        return json_decode( $result );
+    }
+
+    /**
+     * Retrieves data from given endpoint. Does not require the session id to be placed in the data.
+     * @param string $endpoint
+     * @param mixed $data
+     */
+    public function get( $endpoint, $data=array() )
+    {
+        $data = array_merge( array( 'session' => $this->_session->key ), $data );
+        return $this->getData( $this->_session->server, $endpoint, $data);
+    }
+
+    /**
+     * Formats given requests and retrieves data from the endpoint Poll
+     * @param mixed $requests
+     * @return mixed
+     */
+    public function poll( $requests )
+    {
+        $data = array( 'requestid' => mt_rand( 1000, 9999 ), 'requests' => '' );
+        foreach( $requests as $key => $val )
+        {
+            $data['requests'] .= strtoupper( $key ) . ':' . $val . '\f';
+        }
+
+        return $this->get( 'Poll', $data );
+    }
+
+    /**
+     * Generates a name for our cookiefile.
+     * @private
+     * @param string
+     * @return string
+     */
+    private function generateCookieFile( $id )
+    {
+        return $this->_cookie_path . md5($id) . 'txt';
+    }
+}
+
